2073ee66ec51db4b93b30c052b2658dfb57add2d
Jonathan Richard <jwrichard26@gmail.com>
2025-06-04 09:42:59 -0400

    Testing on a new approach to registering training/inference functions

---
+++ examples/mnist-heat-main/src/main.rs
@@ -0,0 +1,7 @@
+// uncomment api_endpoint and wss if in dev mode
+#[tracel::heat::macros::heat_cli_main(
+    mnist_heat,
+    // api_endpoint = "http://localhost:9001",
+    // wss = false
+)]
+fn main() {}
