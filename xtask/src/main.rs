42d2ed55aa480b140ddf1042b1f4fed38571ec50
syl20bnr <sylvain.benner@tracel.ai>
2026-08-20 15:13:33 -0400

    chore: update to xtask v5

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -4,7 +4,7 @@ extern crate log;
 use std::time::Instant;
 use tracel_xtask::prelude::*;
 
-#[macros::base_commands(Build, Bump, Check, Compile, Doc, Fix, Publish, Test, Validate)]
+#[macros::base_commands]
 enum Command {}
 
 fn main() -> anyhow::Result<()> {
